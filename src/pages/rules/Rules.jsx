00b547b3daaf91447c1b5a5bb01bb72e1986ef8b
Ahmadov Kamal <ahmadov.kamal423@gmail.com>
2022-11-12 20:36:59 +0400

    added basic routing, navbar layout

---
+++ src/pages/rules/Rules.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+function Rules() {
+  return (
+    <div>
+      <Navbar currentPage={3}/>
+      This is Rules Page
+      </div>
+  )
+}
+
+export default Rules
