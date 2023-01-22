479f2a91093883a5aa0cd064164d3a86690d2eca
Shams Gadirli <shams.kadirli@gmail.com>
2023-01-23 00:46:22 +0400

    fix images imports

--- src/components/card-component/Card.jsx
+++ src/components/card-component/Card.jsx
@@ -1,10 +1,13 @@
 import "./Card.scss";
+import bunny from "../../assets/images/Bunny.png"
+
+
 export function Card({ cardNumber }) {
   return (
     <div className="card-wrapper">
       <p className="top">{cardNumber}</p>
       <p className="bottom">{cardNumber}</p>
-      <img src="src/assets/images/Bunny.png" alt="" />
+      <img src={bunny} alt="" />
     </div>
   );
 }
